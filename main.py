7ebfc2fdd61cd7a6e86ee02e2ccb4492875730f6
VIDAL-Antoine <avidal78390@gmail.com>
2022-04-04 20:13:07 +0200

    Ajout retrait dans le dictionnaire fait

--- main.py
+++ main.py
@@ -33,19 +33,112 @@ def main():
         if choix == 1:
             dico.print_dictionary()
 
-        if choix == 2:
+        elif choix == 2:
             phrase = sentence.Sentence()
             phrase.generate_random_sentence()
             print(f'\nLa phrase générée est la suivante :')
             phrase.print_sentence()
 
-        if choix == 3:
+        elif choix == 3:
             phrase = input("Veuillez tapez une phrase : ")
 
             if(sentence.Sentence.check_is_in_dico(phrase)):
                 print(f'La phrase est bien valide.')
             else:
                 print(f'La phrase n\'est pas valide.')
+
+        elif choix == 4:
+            print("\nSouhaitez-vous ajouter un sujet, un verbe ou un adverbe?\n",
+            "0 - Annuler",
+            "1 - Ajouter un sujet",
+            "2 - Ajouter un verbe",
+            "3 - Ajouter un adverbe",
+            sep = '\n'
+            )
+
+            try:
+                choix = int(input("Veuillez choisir un nombre entre 0 et 3 : "))
+                print(f'\n##### Choix {choix} #####')
+
+            except ValueError:
+                print(f'\nERREUR : la saisie n\'est pas un nombre.\n')
+
+            else:
+                if choix < 0 or choix > 3:
+                    print(f'\nERREUR : Nombre hors portée.\n')
+
+            if choix == 0:
+                choix = -1
+
+            if choix == 1:
+                sjt = input(f'Veuillez saisir le sujet que vous souhaitez ajouter. Nous supposons que vous tapez un sujet qui existe vraiment :\n')
+                if sjt not in dico.subjects:
+                    dico.subjects.append(sjt)
+                    print(f'"{sjt}" ajouté dans les sujets.')
+                else:
+                    print(f'Le sujet "{sjt}" est déjà dans le dictionnaire. Inutile de l\'ajouter.')
+
+            if choix == 2:
+                verb = input(f'Veuillez saisir le verbe que vous souhaitez ajouter. Nous supposons que vous tapez un verbe qui existe vraiment :\n')
+                if verb not in dico.verbs:
+                    dico.verbs.append(verb)
+                else:
+                    print(f'Le verbe "{verb}" est déjà dans le dictionnaire. Inutile de l\'ajouter.')
+
+            if choix == 3:
+                adverb = input(f'Veuillez saisir l\'adverbe que vous souhaitez ajouter. Nous supposons que vous tapez un adverbe qui existe vraiment :\n')
+                if adverb not in dico.adverbs:
+                    dico.adverbs.append(adverb)
+                else:
+                    print(f'L\'adverbe "{adverb}" est déjà dans le dictionnaire. Inutile de l\'ajouter.')
+
+        elif choix == 5:
+            print("\nSouhaitez-vous retirer un sujet, un verbe ou un adverbe?\n",
+            "0 - Annuler",
+            "1 - Retirer un sujet",
+            "2 - Retirer un verbe",
+            "3 - Retirer un adverbe",
+            sep = '\n'
+            )
+
+            try:
+                choix = int(input("Veuillez choisir un nombre entre 0 et 3 : "))
+                print(f'\n##### Choix {choix} #####')
+
+            except ValueError:
+                print(f'\nERREUR : la saisie n\'est pas un nombre.\n')
+
+            else:
+                if choix < 0 or choix > 3:
+                    print(f'\nERREUR : Nombre hors portée.\n')
+
+            if choix == 0:
+                choix = -1
+
+            if choix == 1:
+                print(f'Sujets : {dico.subjects}\n')
+                sjt = input(f'Veuillez saisir le sujet que vous souhaitez retirer. Nous supposons que vous tapez un sujet qui existe vraiment :\n')
+                try:
+                    dico.subjects.remove(sjt)
+                except ValueError:
+                    print(f'"{sjt}" non trouvé dans la liste des sujets. Vérifiez qu\'il s\'y trouve bien.')
+
+            if choix == 2:
+                print(f'Verbes : {dico.verbs}\n')
+                verb = input(f'Veuillez saisir le verbe que vous souhaitez retirer. Nous supposons que vous tapez un verbe qui existe vraiment :\n')
+                try:
+                    dico.verbs.remove(verb)
+                except ValueError:
+                    print(f'"{verb}" non trouvé dans la liste des verbes. Vérifiez qu\'il s\'y trouve bien.')
+
+            if choix == 3:
+                print(f'Adverbes : {dico.adverbs}\n')
+                adverb = input(f'Veuillez saisir l\'adverbe que vous souhaitez retirer. Nous supposons que vous tapez un adverbe qui existe vraiment :\n')
+                print(adverb)
+                try:
+                    dico.adverbs.remove(adverb)
+                except ValueError:
+                    print(f'"{adverb}" non trouvé dans la liste des adverbes. Vérifiez qu\'il s\'y trouve bien.')
 
     print(f'\nMerci d\'avoir utilisé ce dictionnaire! Au revoir!')
 
